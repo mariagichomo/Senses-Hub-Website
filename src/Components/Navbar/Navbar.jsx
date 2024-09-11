6c67bceeb67c3336a8cac05447be6d472066fa51
mariagichomo <mariagichomo22@gmail.com>
2024-09-11 14:27:42 +0300

    Styling Navigation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/Senses Hub Logo.png'
 
 const Navbar = () => {
     return (
         <nav>
-
+            <img src ={logo} alt= "Senses Hub Logo"/>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>Events</li>
+                <li>Shop</li>
+            </ul>
         </nav>
     )
 }
