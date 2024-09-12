4e05cfe7ca90ec299db272c3929c8ceaef90b8fb
mariagichomo <mariagichomo22@gmail.com>
2024-09-12 18:30:44 +0300

    updated navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,14 +4,15 @@ import logo from '../../assets/Senses Hub Logo.png'
 
 const Navbar = () => {
     return (
-        <nav>
-            <img src ={logo} alt= "Senses Hub Logo"/>
+        <nav className='container'>
+            <img src ={logo} alt= "Senses Hub Logo" className = 'logo' />
             <ul>
                 <li>Home</li>
                 <li>About Us</li>
                 <li>Services</li>
                 <li>Events</li>
                 <li>Shop</li>
+                <li><button className= 'btn'>Contact Us</button></li>
             </ul>
         </nav>
     )
